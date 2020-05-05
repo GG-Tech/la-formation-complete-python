47c7ba942ef00e965f25274a0196d47d600ed4a1
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 21:47:36 +0200

    Add completed exercice

--- exr-001_input-utilisateur/01-sources/recuperer_input.py
+++ exr-001_input-utilisateur/01-sources/recuperer_input.py
@@ -1,2 +1,10 @@
 # Demander le prénom, la ville de naissance et l'âge de la personne qui utilise le script
 # Afficher ces trois informations
+
+name = input("Quel est ton prénom ? : ")
+ville_de_naissance = input("Quel est ta ville de naissance ? : ")
+age = input("Quel est ton âge ? : ")
+
+print(name)
+print(ville_de_naissance)
+print(age)
